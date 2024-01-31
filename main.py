fa3e6d9763b61a1f2bca13ba3a0820343388c7ae
Michael Lustenberger <mic@inofix.ch>
2024-02-01 00:25:56 +0100

    Outline Basic Functionality
    Add all the central aspects to get an idea.

--- main.py
+++ main.py
@@ -2,13 +2,19 @@
 
 import click
 from glob import glob
+import os
 
 @click.command()
-@click.option('--include', '-I', type=str, multiple=True, help='Arbitrary number of files to include and edit as the document, supports wildcards too.')
-@click.option('--add', '-A', type=str, multiple=True, help='Arbitrary additional files to copy to the target dir, supports wildcards too.')
-@click.option('--link', '-L', type=str, multiple=True, help='Arbitrary additional soft-links to create to the target dir, supports wildcards too.')
+@click.option('--end_marker', '-e', type=str, default='%', help='The closing marker to denote a variable in the template (e.g. "%FN%")')
+@click.option('--flavor', '-F', type=str, default='TeX?', help='If the template is `TeX`, and you say so here,an attempt to transform it directly to PDF will be made.')
+@click.option('--include', '-I', type=str, multiple=True, help='Arbitrary additional files to copy to the target dir, supports wildcards too.')
+@click.option('--link', '-l', type=str, multiple=True, help='Arbitrary additional soft-links to create to the target dir, supports wildcards too.')
+@click.option('--dry_run', '-n', type=bool, help='Do not really change anything...')
+@click.option('--output_directory', '-o', type=str, help='Directory to put the output documents into.')
+@click.option('--template', '-t', type=str, multiple=True, help='Arbitrary number of files to edit as the document, supports wildcards too.')
+@click.option('--start_marker', '-s', type=str, default='%', help='The opening marker to denote a variable in the template (e.g. "%FN%")')
 @click.option('--vcf', '-V', type=str, help='The vcards file(s) to get the personalized infos from (wildcards supported).')
-def main(vcf, include, add, link):
+def main(end_marker, flavor, include, link, dry_run, output_directory, template, start_marker, vcf):
     """
     Sometimes it is just necessary to create a series of documents with
     common content but still personalized or slightly customized.
@@ -19,21 +25,38 @@ def main(vcf, include, add, link):
 
     This little piece of software let's you do it your way.
     """
-    in_file_list = []
-    vc_file_list = []
+    include_file_list = []
+    template_file_list = []
+    template_file_list = []
+    vcf_file_list = []
     for i in include:
         for f in glob(i):
-            in_file_list.append(f)
-    for a in add:
-        print(a)
+            include_file_list.append(f)
+    for t in template:
+        for f in glob(t):
+            template_file_list.append(f)
     for l in link:
-        print(l)
+        for f in glob(l):
+            link_file_list.append(f)
     if vcf:
         for f in glob(vcf):
             vc_file_list.append(f)
     else:
         click.echo("Please provide a vcard file.")
         raise SystemExit
+
+    if not dry_run:
+        organize_output(output_directory)
+
+        if flavor == "TeX":
+            pass
+
+def organize_output(output_directory):
+    try:
+        os.makedirs(output_directory)
+    except FileExistsError:
+        pass
+
 
 def parse_vcf(filename, addresslist=[]) -> list:
     return addresslist
